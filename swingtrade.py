dcad3e518e291ba272a00e9eb0de755014ee44df
justjoshtings <joshting1@gmail.com>
2018-04-03 19:05:01 -0400

    Added price saving implementation

--- swingtrade.py
+++ swingtrade.py
@@ -1,5 +1,8 @@
 
 import os
+from numpy import genfromtxt
+import numpy
+
 def addToClipBoard(text):
     command = 'echo ' + text.strip() + '| pbcopy'
     os.system(command)
@@ -13,28 +16,41 @@ def swingit(lastprice, delta): #Calculate swing prices
 	return
 
 def current_profit(current_eth, current_eth_value): #Calculate profit
-	initial_invest = 0.1062365+0.00133461
+	initial_invest = 0.1062365+0.00133461+0.25577442
 	profit = current_eth - initial_invest
 	percent_profit = (profit/initial_invest)*100
 	dollar_value = profit*current_eth_value
 	print("Profit (ETH): {} | Percent Profit: {} | Dollar Value: {}".format(profit, percent_profit, dollar_value))
 
+def write_current_eth_value(eth_price_list):
+	current_eth_value = float(input("Current ETH price: "))
+	new_eth_price_list = numpy.append(eth_price_list, current_eth_value)
+	print(new_eth_price_list)
+	numpy.savetxt('ETH_price.csv', new_eth_price_list, delimiter = ',')
+	return print("Sucess!")
+
+def current_eth_value_insys():
+	eth_price_list = numpy.loadtxt('ETH_price.csv', delimiter=',')
+	return eth_price_list
+	
 
 def main(): 
-	option = int(input("Swingtrade or Profit?\n1 or 2: "))
+	eth_price_list = current_eth_value_insys()
+	current_eth_value = eth_price_list[-1]
+	option = int(input("Swingtrade or Profit or Enter ETH Value?\n1 or 2 or 3: "))
 	if option == 1:
 		lastprice = float(input("Buy in: "))
 		delta = float(input("Percent Increase: "))
 		swingit(lastprice, delta)
 	elif option == 2:
-		# current_eth_value = 458
-		current_eth_value = 385
+		print('ETH PRICE: {}'.format(current_eth_value))
 		current_eth_1 = float(input("Current ETH: "))
 		current_eth_2 = float(input("Pool ETH: "))
 		current_eth = current_eth_1+current_eth_2
 		current_profit(current_eth, current_eth_value)
 	elif option == 3:
-		get_bitcoin()
+		current_eth_value = write_current_eth_value(eth_price_list)
+		
 
 main()
 
