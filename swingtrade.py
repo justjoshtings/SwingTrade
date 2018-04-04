7b7260498252781fd8be842a97c407d0659f5752
justjoshtings <joshting1@gmail.com>
2018-04-04 13:46:44 -0400

    Minor updates

--- swingtrade.py
+++ swingtrade.py
@@ -32,25 +32,39 @@ def write_current_eth_value(eth_price_list):
 def current_eth_value_insys():
 	eth_price_list = numpy.loadtxt('ETH_price.csv', delimiter=',')
 	return eth_price_list
-	
+
+def save_for_day(current_eth_value):
+	last_eth_value_lst = numpy.loadtxt('profit.csv', delimiter=',')
+	last_eth_value = last_eth_value_lst[-1]
+
+	current_eth_1 = float(input("Current ETH: "))
+	current_eth_2 = float(input("Pool ETH: "))
+	total_eth_today = current_eth_1 + current_eth_2
+	profit_list = numpy.append(last_eth_value_lst, total_eth_today)
+	numpy.savetxt('profit.csv', profit_list, delimiter = ',')
+
+	profit_since_last = total_eth_today - last_eth_value
+	dollar_value = profit_since_last*current_eth_value
+
+	print("Today's Profits\n-----------------\nETH Profit: {} | Percent Profit: {} | Dollar Value: {}".format(profit_since_last, (profit_since_last/last_eth_value)*100, dollar_value))
+	return
 
 def main(): 
 	eth_price_list = current_eth_value_insys()
 	current_eth_value = eth_price_list[-1]
-	option = int(input("Swingtrade or Profit or Enter ETH Value?\n1 or 2 or 3: "))
-	if option == 1:
+	option = int(input("1. Swingtrade | 2. Profit | 3. Enter ETH Value | 4. End of Day\n1 or 2 or 3: "))
+	if option == 1: #Swingtrade
 		lastprice = float(input("Buy in: "))
 		delta = float(input("Percent Increase: "))
 		swingit(lastprice, delta)
-	elif option == 2:
+	elif option == 2: #Calculate Total Profits
 		print('ETH PRICE: {}'.format(current_eth_value))
 		current_eth_1 = float(input("Current ETH: "))
 		current_eth_2 = float(input("Pool ETH: "))
 		current_eth = current_eth_1+current_eth_2
 		current_profit(current_eth, current_eth_value)
-	elif option == 3:
+	elif option == 3: #Enter Current ETH Price
 		current_eth_value = write_current_eth_value(eth_price_list)
-		
-
+	elif option == 4: #Save today's ETH Progress
+		save_for_day(current_eth_value)
 main()
-
